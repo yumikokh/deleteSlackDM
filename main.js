8c999bea89588974558f8051f42658bc8a4a2f21
kokubu-yumiko <kokubu-yumiko@kayac.com>
2016-08-02 22:37:41 +0900

    以降のメッセージをけします

--- main.js
+++ main.js
@@ -7,13 +7,17 @@ imports('./config.js');
 // 消したい直近のメッセージ数
 var AMOUNT = 1;
 
+// ここで指定したタイムスタンプ以降を消す
+var OLDEST_TS = 1467362525.000032;
+
 slack = new Slack(API_TOKEN);
 var ts = [];
 
 // 取得します
 slack.api("im.history", {
   channel: CHANNEL_ID,
-  count: AMOUNT
+  // count: AMOUNT
+  oldest: OLDEST_TS
 }, function(err, response) {
   console.log("取得",response.messages);
   for(var i=0; i < response.messages.length; i++) {
